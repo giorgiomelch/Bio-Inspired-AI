8aaefd107eb54e746215df5b61772a91285da944
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-02-05 19:51:15 +0100

    modifiche per il problema sin

---
+++ project1/IT3708  - Files for Project 1/sin_problem/fitness_function.jl
@@ -0,0 +1,15 @@
+function evaluate_population(pop::Matrix{Bool})
+    n, m = size(pop)
+    fitness_values = zeros(n)
+    for i in 1:n
+        # Convertire il bitstring da Bool a Stringa binaria
+        bitstring = join(pop[i, :] .|> Int)  # Converti ogni Bool in Int e poi unisci in una stringa
+        decimal_value = parse(Int, bitstring; base=2)  # Converti la stringa in un numero decimale
+
+        max_value = 2^m - 1  # Valore massimo possibile con m bit
+        scaled_value = (decimal_value / max_value) * 128  # Scala il valore nell'intervallo [0, 128]
+
+        fitness_values[i] = sin(scaled_value) 
+    end
+    return fitness_values
+end
