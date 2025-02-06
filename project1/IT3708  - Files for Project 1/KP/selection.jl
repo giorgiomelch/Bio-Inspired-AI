7a577378f77497fb7c40f51a07d5d1152d0417b2
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-02-06 15:01:32 +0100

    aggiunto roulette_wheel_selection

--- project1/IT3708  - Files for Project 1/KP/selection.jl
+++ project1/IT3708  - Files for Project 1/KP/selection.jl
@@ -15,3 +15,24 @@ function elitism(fitness::Vector{Int64}, num_parents::Int64)
     best_indices = sortperm(fitness, rev=true)[1:num_parents]
     return best_indices
 end
+
+function roulette_wheel_selection(fitness_values::Vector{Float64}, num_parents::Int64)
+    num_parents = min(num_parents, length(fitness_values))  # Evita problemi di overflow
+    total_fitness = sum(fitness_values)
+    probabilities = fitness_values ./ total_fitness  # Normalizzazione
+
+    cumulative_probabilities = cumsum(probabilities)
+    sub_set = Int[]  # Lista vuota di interi
+    
+    while length(sub_set) < num_parents
+        r = rand()
+        for (i, cp) in enumerate(cumulative_probabilities)
+            if r <= cp && !(i in sub_set)
+                push!(sub_set, i)
+                break  # Esci dal loop una volta trovato un individuo
+            end
+        end
+    end
+    
+    return sub_set
+end
