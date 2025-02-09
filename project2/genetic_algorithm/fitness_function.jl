4d1a6448cf5336be1f5764c2fd99e0ed0aaaed2e
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-02-09 23:24:42 +0100

    first commit of new fitness function

--- project2/genetic_algorithm/fitness_function.jl
+++ project2/genetic_algorithm/fitness_function.jl
@@ -1,16 +1,39 @@
-function evaluate_population(pop::Matrix{Bool})
-    n, m = size(pop)
-    fitness_values = zeros(n)
-    for i in 1:n
-        # Convertire il bitstring da Bool a Stringa binaria
-        bitstring = join(pop[i, :] .|> Int)  # Converti ogni Bool in Int e poi unisci in una stringa
-        decimal_value = parse(Int, bitstring; base=2)  # Converti la stringa in un numero decimale
+function calculate_fitness!(population::Population, problem::HomeCareRoutingProblem)
+    # Estrai la matrice dei tempi di viaggio
+    travel_times = problem.travel_times
 
-        max_value = 2^m - 1  # Valore massimo possibile con m bit
-        scaled_value = (decimal_value / max_value) * 128  # Scala il valore nell'intervallo [0, 128]
-        println(scaled_value)
+    # Itera su ogni individuo nella popolazione
+    for individual in population.population
+        route = individual.route
+        total_time = 0.0
 
-        fitness_values[i] = sin(scaled_value) 
+        # Tempo di partenza dal deposito
+        current_time = route.start_time
+
+        # Se ci sono pazienti nella route
+        if !isempty(route.patients)
+            # Tempo di viaggio dal deposito al primo paziente
+            first_patient = route.patients[1]
+            total_time += travel_times[1, first_patient.id + 1]  # +1 perché il deposito è l'indice 1
+
+            # Tempo di cura del primo paziente
+            total_time += first_patient.care_time
+
+            # Itera sui pazienti successivi
+            for i in 2:length(route.patients)
+                prev_patient = route.patients[i-1]
+                current_patient = route.patients[i]
+                # Tempo di viaggio tra i pazienti
+                total_time += travel_times[prev_patient.id + 1, current_patient.id + 1]
+                # Tempo di cura del paziente corrente
+                total_time += current_patient.care_time
+            end
+
+            # Tempo di viaggio dall'ultimo paziente al deposito
+            last_patient = route.patients[end]
+            total_time += travel_times[last_patient.id + 1, 1]
+        end
+        # Aggiorna la fitness dell'individuo
+        individual.fitness = total_time
     end
-    return fitness_values
 end
