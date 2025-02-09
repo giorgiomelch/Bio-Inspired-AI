7abc066829a43c1ac290a24f6173921021e700a6
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-02-09 23:24:49 +0100

    tests

---
+++ project2/genetic_algorithm/dacanc.jl
@@ -0,0 +1,12 @@
+include("structs.jl")
+include("parser.jl")
+include("initialization.jl")
+include("fitness_function.jl")
+
+a = load_home_care_problem("/home/giorgiomelch/BI_AI/workspace/genetic-algorithm/project2/data/train_1.json")
+b = initialize_population(a)
+println(b.population[1].fitness)
+calculate_fitness!(b, a)
+for i in b.population
+    print(i.fitness," ")
+end
