478d03294cdcc08875b8491b36191d5741b91c63
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-02-17 16:30:03 +0100

    order1_crossover draft

--- project2/genetic_algorithm/crossover.jl
+++ project2/genetic_algorithm/crossover.jl
@@ -1,5 +1,69 @@
 using Random
 
+function order1_crossover(parent1::Individual, parent2::Individual)
+    # Estrai la sequenza di pazienti da entrambi i genitori
+    p1_patients = parent1.route.patients
+    p2_patients = parent2.route.patients
+    
+    if length(p1_patients) != length(p2_patients)
+        error("I genitori devono avere lo stesso numero di pazienti")
+    end
+    
+    n = length(p1_patients)
+    
+    # Seleziona un segmento casuale
+    start_idx = rand(1:n)
+    end_idx = rand(start_idx:n)
+    
+    # Copia la sottosequenza dal primo genitore
+    child_patients = fill(nothing, n)
+    child_patients[start_idx:end_idx] = p1_patients[start_idx:end_idx]
+    
+    # Riempie gli slot vuoti mantenendo l'ordine dal secondo genitore
+    idx = 1
+    for p in p2_patients
+        if p ∉ child_patients
+            while child_patients[idx] !== nothing
+                idx += 1
+            end
+            child_patients[idx] = p
+        end
+    end
+    
+    # Crea un nuovo individuo figlio con la nuova route
+    child_route = Route(parent1.route.nurse, parent1.route.depot_return_time)
+    child_route.patients = child_patients
+    
+    return Individual(child_route)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function two_point_crossover(population::Matrix{Bool})
     num_individuals, num_genes = size(population)
     # Creiamo una nuova popolazione per contenere i figli
