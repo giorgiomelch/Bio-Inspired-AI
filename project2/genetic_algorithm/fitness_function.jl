01c20086417f4008c58475cc8f58794601f9d999
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-02-18 19:49:55 +0100

    tieni conto dei costrain e aggiungi penalità

--- project2/genetic_algorithm/fitness_function.jl
+++ project2/genetic_algorithm/fitness_function.jl
@@ -1,6 +1,8 @@
 function calculate_route_time(route, travel_times)
     total_time = 0.0
+    is_feasible = true
     if !isempty(route.patients)
+        nurse_capicity = route.nurse.capacity
         # Tempo di viaggio dal deposito al primo paziente
         first_patient = route.patients[1]
         total_time += travel_times[1, first_patient.id + 1] + first_patient.care_time
@@ -10,10 +12,21 @@ function calculate_route_time(route, travel_times)
             current_patient = route.patients[i]
             total_time += travel_times[prev_patient.id + 1, current_patient.id + 1] # Tempo di viaggio tra i pazienti
             total_time += current_patient.care_time # Tempo di cura del paziente corrente
+            if total_time < current_patient.start_time || total_time > current_patient.end_time
+                is_feasible = false
+            end
+            nurse_capicity -= current_patient.demand
+            if nurse_capicity < 0
+                is_feasible = false
+            end
         end
         # Tempo di viaggio dall'ultimo paziente al deposito
         last_patient = route.patients[end]
         total_time += travel_times[last_patient.id + 1, 1]
+        # PENALITA' SE I COSTRAINS NON SONO RISPETTATI
+        if !is_feasible
+            total_time *=2
+        end
     end
     return total_time
 end
