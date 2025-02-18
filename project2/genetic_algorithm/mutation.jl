84587e251953d7316cf271734d88bd78ae8e4600
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-02-18 22:41:27 +0100

    aggiunta mutazione inversa

--- project2/genetic_algorithm/mutation.jl
+++ project2/genetic_algorithm/mutation.jl
@@ -1,4 +1,4 @@
-function mutation_swap!(individual::Individual, N_GEN_SWAP_MUTATION::Int)
+function mutation_swap!(individual::Individual, N_GEN_SWAP_MUTATION::Int64)
     n_routes = length(individual.routes)
     for _ in 1:N_GEN_SWAP_MUTATION
         #scegli una rotta a caso
@@ -11,9 +11,21 @@ function mutation_swap!(individual::Individual, N_GEN_SWAP_MUTATION::Int)
         end
     end
 end
-
-function apply_mutation!(population::Population, N_GEN_SWAP_MUTATION::Int)
+function mutation_inversion!(individual::Individual, N_GEN_INVERSION::Int64)
+    n_routes = length(individual.routes)
+    for _ in 1:N_GEN_INVERSION
+        # Scegli una rotta a caso
+        route = individual.routes[rand(1:n_routes)]
+        if length(route.patients) > 2
+            # Seleziona un intervallo casuale e invertilo
+            i, j = sort(rand(1:length(route.patients), 2)) 
+            route.patients[i:j] = reverse(route.patients[i:j])
+        end
+    end
+end
+function apply_mutation!(population::Population, N_GEN_SWAP_MUTATION::Int64)
     for individual in population.individuals
         mutation_swap!(individual, N_GEN_SWAP_MUTATION)
+        mutation_inversion!(individual, N_GEN_SWAP_MUTATION)
     end
 end
