648b5ae132d21267b3a43b37d740656721a1d30e
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-02-18 15:34:59 +0100

    mutation function

--- project2/genetic_algorithm/main.jl
+++ project2/genetic_algorithm/main.jl
@@ -2,22 +2,22 @@ include("structs.jl")
 include("parser.jl")
 include("initialization.jl")
 include("fitness_function.jl")
+include("mutation.jl")
 
 
 #COSTANTS
 N_POP = 1_000
-N_GEN_MUTATION = 1
+N_GEN_SWAP_MUTATION = 1
 POP_REPLACEMENT = 0.5
 
 
 # GENETIC ALGORITHM
 a = load_home_care_problem("/home/giorgiomelch/BI_AI/workspace/genetic-algorithm/project2/data/train_1.json")
 b = initialize_pop_random(a, N_POP)
-println(b.individuals[1].fitness)
 update_population_fitness!(b, a)
-for i in b.individuals
-    print(i.fitness," ")
-end
+println("\n", b.best_individual.fitness)
+apply_mutation!(b, N_GEN_SWAP_MUTATION)
+update_population_fitness!(b, a)
 println("\n", b.best_individual.fitness)
 
 
@@ -32,7 +32,8 @@ function genetic_algorithm(problem::HomeCareRoutingProblem, N_POP::Int, N_GEN_MU
     calculate_fitness!(population, problem)
     for _ in 1:50 #termination criterion TO DO
         #CROSSOVER TO DO
-        #MUTATION TO DO
+        #MUTZIONE
+        apply_mutation!(population, N_GEN_SWAP_MUTATION)
         #SELEZIONE SURVIVORS TO DO
         
         #MAPPA I PROGRESSI
--- project2/genetic_algorithm/mutation.jl
+++ project2/genetic_algorithm/mutation.jl
@@ -1,13 +1,19 @@
-function mutation_max_n_genes!(curr_pop::Matrix{Bool}, mutation_probability::Float64, n::Int64)
-    num_rows, num_cols = size(curr_pop)
-    # Per ogni genitore nella popolazione
-    for i in 1:num_rows
-        gene_to_mutate = randperm(num_cols)[1:n]  # Seleziona n geni casuali
-        # Applica la mutazione con la probabilità specificata
-        for j in eachindex(gene_to_mutate)
-            if rand() < mutation_probability
-                curr_pop[i, gene_to_mutate[j]] = !curr_pop[i, gene_to_mutate[j]]  # Inverte il valore del gene
-            end
+function mutation_swap!(individual::Individual, N_GEN_SWAP_MUTATION::Int)
+    n_routes = length(individual.routes)
+    for _ in 1:N_GEN_SWAP_MUTATION
+        #scegli una rotta a caso
+        route = individual.routes[rand(1:n_routes)]
+        if length(route.patients) > 2
+            #cambia due pazienti di posto
+            i = rand(1:length(route.patients))
+            j = rand(1:length(route.patients))
+            route.patients[i], route.patients[j] = route.patients[j], route.patients[i]
         end
+    end
+end
+
+function apply_mutation!(population::Population, N_GEN_SWAP_MUTATION::Int)
+    for individual in population.individuals
+        mutation_swap!(individual, N_GEN_SWAP_MUTATION)
     end
 end
