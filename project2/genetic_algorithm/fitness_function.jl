cbd8d9fe1f91c997718c1fdd9c785f620551ed27
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-02-18 15:02:57 +0100

    fitness function update

--- project2/genetic_algorithm/fitness_function.jl
+++ project2/genetic_algorithm/fitness_function.jl
@@ -1,39 +1,29 @@
-function calculate_fitness!(population::Population, problem::HomeCareRoutingProblem)
-    # Estrai la matrice dei tempi di viaggio
-    travel_times = problem.travel_times
-
-    # Itera su ogni individuo nella popolazione
-    for individual in population.individuals   
-        route = individual.route
-        total_time = 0.0
-
-        # Tempo di partenza dal deposito
-        current_time = route.start_time
-
-        # Se ci sono pazienti nella route
-        if !isempty(route.patients)
-            # Tempo di viaggio dal deposito al primo paziente
-            first_patient = route.patients[1]
-            total_time += travel_times[1, first_patient.id + 1]  # +1 perché il deposito è l'indice 1
-
-            # Tempo di cura del primo paziente
-            total_time += first_patient.care_time
-
-            # Itera sui pazienti successivi
-            for i in 2:length(route.patients)
-                prev_patient = route.patients[i-1]
-                current_patient = route.patients[i]
-                # Tempo di viaggio tra i pazienti
-                total_time += travel_times[prev_patient.id + 1, current_patient.id + 1]
-                # Tempo di cura del paziente corrente
-                total_time += current_patient.care_time
-            end
-
-            # Tempo di viaggio dall'ultimo paziente al deposito
-            last_patient = route.patients[end]
-            total_time += travel_times[last_patient.id + 1, 1]
+function calculate_route_time(route, travel_times)
+    total_time = 0.0
+    if !isempty(route.patients)
+        # Tempo di viaggio dal deposito al primo paziente
+        first_patient = route.patients[1]
+        total_time += travel_times[1, first_patient.id + 1] + first_patient.care_time
+        # Itera sui pazienti della rotta
+        for i in 2:length(route.patients)
+            prev_patient = route.patients[i - 1]
+            current_patient = route.patients[i]
+            total_time += travel_times[prev_patient.id + 1, current_patient.id + 1] # Tempo di viaggio tra i pazienti
+            total_time += current_patient.care_time # Tempo di cura del paziente corrente
         end
-        # Aggiorna la fitness dell'individuo
-        individual.fitness = total_time
+        # Tempo di viaggio dall'ultimo paziente al deposito
+        last_patient = route.patients[end]
+        total_time += travel_times[last_patient.id + 1, 1]
+    end
+    return total_time
+end
+
+function update_population_fitness!(population::Population, problem::HomeCareRoutingProblem)
+    travel_times = problem.travel_times
+    for individual in population.individuals
+        individual.fitness = sum(calculate_route_time(route, travel_times) for route in individual.routes)
+        if individual.fitness < population.best_individual.fitness # aggiorna se migliore
+            population.best_individual = individual
+        end
     end
 end
--- project2/genetic_algorithm/main.jl
+++ project2/genetic_algorithm/main.jl
@@ -7,13 +7,34 @@ include("fitness_function.jl")
 #COSTANTS
 N_POP = 1_000
 N_GEN_MUTATION = 1
+POP_REPLACEMENT = 0.5
 
 
 # GENETIC ALGORITHM
 a = load_home_care_problem("/home/giorgiomelch/BI_AI/workspace/genetic-algorithm/project2/data/train_1.json")
 b = initialize_pop_random(a, N_POP)
 println(b.individuals[1].fitness)
-calculate_fitness!(b, a)
+update_population_fitness!(b, a)
 for i in b.individuals
     print(i.fitness," ")
+end
+println("\n", b.best_individual.fitness)
+
+
+
+
+
+
+
+
+function genetic_algorithm(problem::HomeCareRoutingProblem, N_POP::Int, N_GEN_MUTATION::Int, POP_REPLACEMENT::Float64)
+    population = initialize_pop_random(problem, N_POP)
+    calculate_fitness!(population, problem)
+    for _ in 1:50 #termination criterion TO DO
+        #CROSSOVER TO DO
+        #MUTATION TO DO
+        #SELEZIONE SURVIVORS TO DO
+        
+        #MAPPA I PROGRESSI
+    end
 end
