f5ec76eb0b98c4ee99b7da1b309c85f7fdef4795
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-02-18 19:52:12 +0100

    elitism e tournament aggiunti

--- project2/genetic_algorithm/selection.jl
+++ project2/genetic_algorithm/selection.jl
@@ -1,38 +1,22 @@
-function tournament_selection(fitness::Vector{Float64}, num_parents::Int, tournament_size::Int)
-    parents = Vector{Int}(undef, num_parents)
-    for i in 1:num_parents
-        # Seleziona un sottoinsieme casuale della popolazione
-        candidates = randperm(length(fitness))[1:tournament_size]
-        # Trova l'individuo con il fitness massimo tra i candidati
-        best_index = argmax(fitness[candidates])
-        parents[i] = candidates[best_index]
-    end
-    return parents
+function elitism!(population::Population)
+    # Ordina la popolazione in base al fitness
+    sort!(population.individuals, by = x -> x.fitness)
+    # scarta i peggiori individui
+    population.individuals = population.individuals[1:population.N_POP]
 end
 
-function elitism(fitness::Vector{Float64}, num_parents::Int64)
-    # Ordina i fitoness in ordine decrescente e seleziona i migliori
-    best_indices = sortperm(fitness, rev=true)[1:num_parents]
-    return best_indices
-end
-
-function roulette_wheel_selection(fitness_values::Vector{Float64}, num_parents::Int64)
-    num_parents = min(num_parents, length(fitness_values))  # Evita problemi di overflow
-    total_fitness = sum(fitness_values)
-    probabilities = fitness_values ./ total_fitness  # Normalizzazione
-
-    cumulative_probabilities = cumsum(probabilities)
-    sub_set = Int[]  # Lista vuota di interi
-    
-    while length(sub_set) < num_parents
-        r = rand()
-        for (i, cp) in enumerate(cumulative_probabilities)
-            if r <= cp && !(i in sub_set)
-                push!(sub_set, i)
-                break  # Esci dal loop una volta trovato un individuo
-            end
-        end
+function tournament_selection(population::Population, num_survivors::Int, tournament_size::Int)
+    survivors = Vector{Individual}(undef, num_survivors)
+    for i in 1:num_survivors
+        # Seleziona un sottoinsieme casuale di individui
+        candidates_indices = randperm(population.N_POP)[1:tournament_size]
+        individuals_in_tournament = [population.individuals[idx] for idx in candidates_indices]
+        # Trova l'individuo con il fitness massimo
+        best_individual = argmax(ind -> ind.fitness, individuals_in_tournament)
+        # Aggiungi il miglior individuo alla lista dei genitori
+        survivors[i] = best_individual
     end
     
-    return sub_set
+    return survivors
 end
+
