bc84d5c9cf221b965a0a449ea51f493b63d1e043
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-02-18 22:00:05 +0100

    plot grafo

--- project2/plotter/plots.jl
+++ project2/plotter/plots.jl
@@ -1,19 +1,33 @@
 using Plots
+using Random
 
-function plot_fitness_evolution(mean_fitness::Vector{Float64}, max_fitness::Vector{Float64}, min_fitness::Vector{Float64})
+function plot_routes(depot, patients, routes)
+    colors = distinguishable_colors(length(routes))
+    
     # Creazione del grafico
-    p = plot(1:length(mean_fitness), mean_fitness, label="Mean", lw=2, color=:blue, linestyle=:solid)  # Media
-    plot!(p, 1:length(max_fitness), max_fitness, label="Maximum", lw=2, color=:green, linestyle=:solid)  # Massimo
-    plot!(p, 1:length(min_fitness), min_fitness, label="Minimum", lw=2, color=:red, linestyle=:solid)  # Minimo
-    xlabel!(p, "Iteration")  # Etichetta asse x
-    ylabel!(p, "Fitness")  # Etichetta asse y
-    title!(p, "Fitness Evolution")  # Titolo
-    display(p)  # Mostra il grafico finale
+    p = scatter([depot[:x]], [depot[:y]], markershape=:circle, label="Depot", markersize=15, color=:black)
+    scatter!(p, [p[:x] for p in patients], [p[:y] for p in patients], markershape=:square, label="Patients", markersize=3, color=:black)
+    
+    for (i, route) in enumerate(routes)
+        if !isempty(route)
+            route_x = [depot[:x]; [patients[p][:x] for p in route]; depot[:x]]
+            route_y = [depot[:y]; [patients[p][:y] for p in route]; depot[:y]]
+            plot!(p, route_x, route_y, lw=2, label="Nurse $(i)", color=colors[i])
+        end
+    end
+    
+    # Impostazioni del grafico
+    title!("Home Care Routing Solution")
+    plot!(p, legend=:topright, axis=false, grid=false)
+    
+    # Mostra il grafico
+    display(p)
 end
-function plot_humming_distance(distance::Vector{Float64})
-    p = plot(1:length(distance), distance, label="Distance", lw=2, color=:black, linestyle=:solid)  # Distance
-    xlabel!(p, "Iteration")  # Etichetta asse x
-    ylabel!(p, "Distance")  # Etichetta asse y
-    title!(p, "Distance Evolution")  # Titolo
-    display(p)  # Mostra il grafico finale
-end
+
+# Esempio di dati
+depot = Dict(:x => 0, :y => 0)
+patients = [Dict(:x => rand(-10:10), :y => rand(-10:10)) for _ in 1:10]
+routes = [[1, 3, 5], [2, 4, 6, 8], [7, 9, 10]]
+
+# Plot delle rotte
+plot_routes(depot, patients, routes)
