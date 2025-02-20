1fa4b78fb59547145b6f5de743645ddac5fb8939
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-02-20 15:00:16 +0100

    main updated

--- project2/genetic_algorithm/main.jl
+++ project2/genetic_algorithm/main.jl
@@ -6,45 +6,20 @@ include("mutation.jl")
 include("selection.jl")
 include("../plotter/plots.jl")
 include("debug_time.jl")
+include("genetic_algorithm.jl")
 using Statistics
 
 #COSTANTS
 N_POP = 1_000
 POP_REPLACEMENT = 0.5
 # COSTANTS FOR MUTATION
-N_GEN_SWAP_MUTATION = 1
-N_GEN_INVERSION = 1
-
-function genetic_algorithm(
-    problem::HomeCareRoutingProblem, N_POP::Int, POP_REPLACEMENT::Float64, 
-    N_GEN_SWAP_MUTATION::Int64, N_GEN_INVERSION::Int64)
-
-    population = knn_initialize_population(problem, N_POP)
-    update_population_fitness!(population, problem)
-    for _ in 1:50 # TO DO - termination criterion - TO DO
-        #SELZIONE GENITORI PER CROSSOVER
-        indi = tournament_selection(population, Int(N_POP*POP_REPLACEMENT), 10)
-        # TO DO - CROSSOVER - TO DO
-        # crossover!(indi)
-        # aggiungi i figli alla popolazione
-        #MUTAZIONE
-        apply_mutation!(population, N_GEN_SWAP_MUTATION, N_GEN_INVERSION)
-        update_population_fitness!(population, problem)
-        #SELEZIONE SURVIVORS
-        elitism!(population)
-        
-        # MAPPA I PROGRESSI
-        push_mean_fitness!(population)
-        push_min_fitness!(population)
-    end
-    plot_routes2(problem.depot, population.best_individual.routes)
-    plot_fitness_evolution(population)
-
-end
+N_GEN_SWAP_MUTATION = 2
+N_GEN_INVERSION = 2
 
 
-HCP = load_home_care_problem("/home/giorgiomelch/BI_AI/workspace/genetic-algorithm/project2/data/train_1.json")
+
+HCP = load_home_care_problem("/home/giorgiomelch/BI_AI/workspace/genetic-algorithm/project2/data/train_0.json")
 
 # debug()
 
-genetic_algorithm(HCP, N_POP, POP_REPLACEMENT, N_GEN_SWAP_MUTATION, N_GEN_INVERSION)
+@time genetic_algorithm(HCP, N_POP, POP_REPLACEMENT, N_GEN_SWAP_MUTATION, N_GEN_INVERSION)
