11ea3acc816be610b936695cb2daf8ded9a1d0bb
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-02-21 16:38:49 +0100

    roba

--- Home_Care_Problem/genetic_algorithm/main.jl
+++ Home_Care_Problem/genetic_algorithm/main.jl
@@ -1,20 +1,21 @@
 include("structs.jl")
 include("parser.jl")
+include("genetic_algorithm.jl")
 include("initialization.jl")
 include("fitness_function.jl")
-include("mutation.jl")
 include("selection.jl")
+include("crossover.jl")
+include("mutation.jl")
 include("../plotter/plots.jl")
 include("debug_time.jl")
-include("genetic_algorithm.jl")
 using Statistics
 ###############
 #   COSTANTS  #
 ###############
 # TERMINATION CRITERION
-N_ITER = 500
+N_ITER = 5_0000
 # POP CONSTANTS
-N_POP = 5_000
+N_POP = 5_00
 POP_REPLACEMENT = 0.5
 # TORUNAMENT SELECTION CONSTANTS
 TOURNAMENT_SIZE = 10
@@ -24,9 +25,9 @@ N_GEN_SWAP_MUTATION = 1
 N_GEN_INVERSION = 1
 N_GEN_SHIFT = 1
 
+data_train_nbr = 0
 
-
-HCP = load_home_care_problem("/home/giorgiomelch/BI_AI/workspace/genetic-algorithm/project2/data/train_0.json")
+HCP = load_home_care_problem("/home/giorgiomelch/BI_AI/workspace/genetic-algorithm/Home_Care_Problem/data/train_"* string(data_train_nbr) *".json")
 
 # debug()
 
