750504208bfed6c13fd28ecad7a164f7d0608491
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-02-27 18:09:58 +0100

    update

--- Home_Care_Problem/genetic_algorithm/main.jl
+++ Home_Care_Problem/genetic_algorithm/main.jl
@@ -12,9 +12,9 @@ using Statistics
 #   COSTANTS  #
 ###############
 # TERMINATION CRITERION
-N_ITER = 20_000
+N_ITER = 10_000
 # POP CONSTANTS
-N_POP = 500
+N_POP = 1_000
 POP_REPLACEMENT = 0.8
 # TORUNAMENT SELECTION CONSTANTS
 TOURNAMENT_SIZE = 20
@@ -34,5 +34,8 @@ HCP = load_home_care_problem("/home/giorgiomelch/BI_AI/workspace/genetic-algorit
         N_GEN_SWAP_MUTATION, N_GEN_INVERSION, N_GEN_SHIFT)
 
 print_individual_routes(best_individual)
-println("Best fitness: ", best_individual.fitness, ", feasible: ", best_individual.feasible, "\ntime_windows_respected: ", all(r -> r.time_windows_respected, best_individual.routes), "\ncapacity_respected: ", all(r -> r.capacity_respected, best_individual.routes), "\ntime_windows_respected: ", all(r -> r.time_windows_respected, best_individual.routes))
+println("Best fitness: ", best_individual.fitness, ", feasible: ", best_individual.feasible, "\nis_back_before_return_time: ", all(r -> r.is_back_before_return_time, best_individual.routes), "\ncapacity_respected: ", all(r -> r.capacity_respected, best_individual.routes), "\ntime_windows_respected: ", all(r -> r.time_windows_respected, best_individual.routes))
+
 println("\nBenchmark: ", HCP.benchmark)
+#PROVA A FARE MUTATION INVERSION SOLO NELLE ROTTE CON rime time_windows_respected false
+# MUTAZIONE CHE PRENDE UNA ROTTA E LA SPEZZA IN DUE O DUE ROTTE E LE UNISCE IN UNA SOL
