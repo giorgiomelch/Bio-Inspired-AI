450239131f94bed1bfdcc74f6ce7727a45aa6668
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-03-03 22:05:58 +0100

    modifiche iperparametri

--- Home_Care_Problem/genetic_algorithm/main.jl
+++ Home_Care_Problem/genetic_algorithm/main.jl
@@ -15,10 +15,10 @@ using Statistics
 # TERMINATION CRITERION
 N_ITER = 30_000
 # POP CONSTANTS
-N_POP = 1_000
+N_POP = 500
 POP_REPLACEMENT = 0.8
 # TORUNAMENT SELECTION CONSTANTS
-TOURNAMENT_SIZE = 20
+TOURNAMENT_SIZE = 5
 # CROSSOVER CONSTANTS
 # MUTATION COSTANTS
 N_GEN_SWAP_MUTATION = 4
@@ -26,7 +26,7 @@ N_GEN_INVERSION = 1
 N_GEN_SHIFT = 2
 
 data_train_nbr = 0
-for data_train_nbr in 0:1
+for data_train_nbr in 0:9
         HCP = load_home_care_problem("/home/giorgiomelch/BI_AI/workspace/genetic-algorithm/Home_Care_Problem/data/train_"* string(data_train_nbr) *".json")
 
         @time best_individual = genetic_algorithm(HCP, N_POP, POP_REPLACEMENT, 
@@ -41,4 +41,5 @@ for data_train_nbr in 0:1
         save_individual_routes(best_individual, data_train_nbr, "./solution_"*string(data_train_nbr)*".txt", "/home/giorgiomelch/BI_AI/workspace/genetic-algorithm/Home_Care_Problem/solutions")
         # PROVA A FARE MUTATION INVERSION SOLO NELLE ROTTE CON rime time_windows_respected false
         # MUTAZIONE CHE PRENDE UNA ROTTA E LA SPEZZA IN DUE O DUE ROTTE E LE UNISCE IN UNA SOLA
+        # IMPLEMENTARE TORNEO 
 end
