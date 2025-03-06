1f84f4c7b55dee3b83b65e5595092d3aee6da64e
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-03-06 16:29:12 +0100

    just_mutation

--- Home_Care_Problem/genetic_algorithm/genetic_algorithm.jl
+++ Home_Care_Problem/genetic_algorithm/genetic_algorithm.jl
@@ -4,15 +4,16 @@ function genetic_algorithm(
     TOURNAMENT_SIZE::Int,
     N_SWAP::Int64, N_INVERSION::Int64, N_SHIFT::Int64, PERC_SPLIT_MUTATION::Float64)
     
-    #Variabili per adaptive mutation
+    # VARIABILI PER ADAPTIVE MUTATION
     N_SWAP_CURR = N_SWAP
     N_INVERSION_CURR = N_INVERSION
     N_SHIFT_CURR = N_SHIFT
     PERC_SPLIT_MUTATION_CURR = PERC_SPLIT_MUTATION
     fitness_history = []
 
-    population = knn_initialize_population(problem, N_POP, 10)
-    
+    #population = knn_initialize_population(problem, N_POP, 10)
+    population = mixed_initialize_population(problem, N_POP, 10)
+
     update_population_fitness!(population, problem)
     for iter in 1:N_ITER 
         if iter % 2 == 0
@@ -31,12 +32,13 @@ function genetic_algorithm(
         end
         # CROSSOVER
         crossover_pop!(population, problem.travel_times)
+        # MUTATION
         adaptive_mutation!(fitness_history,
             N_SWAP, N_INVERSION, N_SHIFT, PERC_SPLIT_MUTATION,
             N_SWAP_CURR, N_INVERSION_CURR, N_SHIFT_CURR, PERC_SPLIT_MUTATION_CURR)
         apply_mutation!(population, N_SWAP_CURR, N_INVERSION_CURR, N_SHIFT_CURR, PERC_SPLIT_MUTATION_CURR)
         update_population_fitness!(population, problem)
-        #SELEZIONE SURVIVORS
+        # SELEZIONE SURVIVORS
         elitism_tournament_survivor_selection!(population, TOURNAMENT_SIZE)
         # MAPPA I PROGRESSI
         push_mean_fitness!(population)
@@ -45,5 +47,27 @@ function genetic_algorithm(
     end
     plot_routes(problem.depot, population.best_individual.routes)
     plot_fitness_evolution(population)
+    #just_mutation!(population, problem, 10_000, 3, 1, 4, 0.1)
     return population.best_individual
+end
+
+function just_mutation!(population, problem, N_ITER, N_SWAP_CURR, N_INVERSION_CURR, N_SHIFT_CURR, PERC_SPLIT_MUTATION_CURR)
+    for iter in 1:N_ITER 
+        if iter % 100 == 0
+            println("Iterazione: ", iter)
+            println("Mean fitness: ", population.mean_fitness[end])
+            println("Best fitness: ", population.best_individual.fitness, ", feasible: ", population.best_individual.feasible)
+        end
+        # MUTATION
+        apply_mutation!(population, N_SWAP_CURR, N_INVERSION_CURR, N_SHIFT_CURR, PERC_SPLIT_MUTATION_CURR)
+        update_population_fitness!(population, problem)
+        # SELEZIONE SURVIVORS
+        elitism!(population)
+        # MAPPA I PROGRESSI
+        push_mean_fitness!(population)
+        push_min_fitness!(population)
+    end
+
+    plot_routes(problem.depot, population.best_individual.routes)
+    plot_fitness_evolution(population)
 end
