7bc9cff1e9a243090bd0e94fa10a675be1fa74b5
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-03-06 21:32:56 +0100

    aggiunta mutazione move

--- Home_Care_Problem/genetic_algorithm/genetic_algorithm.jl
+++ Home_Care_Problem/genetic_algorithm/genetic_algorithm.jl
@@ -2,9 +2,10 @@ function genetic_algorithm(
     problem::HomeCareRoutingProblem, N_POP::Int, 
     N_ITER::Int,
     TOURNAMENT_SIZE::Int,
-    N_SWAP::Int64, N_INVERSION::Int64, N_SHIFT::Int64, PERC_SPLIT_MUTATION::Float64)
+    N_MOVE::Int64, N_SWAP::Int64, N_INVERSION::Int64, N_SHIFT::Int64, PERC_SPLIT_MUTATION::Float64)
     
     # VARIABILI PER ADAPTIVE MUTATION
+    N_MOVE_CURR = N_MOVE
     N_SWAP_CURR = N_SWAP
     N_INVERSION_CURR = N_INVERSION
     N_SHIFT_CURR = N_SHIFT
@@ -13,10 +14,11 @@ function genetic_algorithm(
 
     #population = knn_initialize_population(problem, N_POP, 10)
     population = mixed_initialize_population(problem, N_POP, 10)
-
     update_population_fitness!(population, problem)
+
     for iter in 1:N_ITER 
         if iter % 2 == 0
+            println("--------------------")
             println("Iterazione: ", iter)
             println("Mean fitness: ", population.mean_fitness[end])
             println("Best fitness: ", population.best_individual.fitness, ", feasible: ", population.best_individual.feasible)
@@ -24,34 +26,35 @@ function genetic_algorithm(
             capacity_respected = all(route -> route.capacity_respected, population.best_individual.routes)
             time_windows_respected = all(route -> route.time_windows_respected, population.best_individual.routes)
             return_time_respected = all(route -> route.is_back_before_return_time, population.best_individual.routes)
-            println("Tutte le rotte rispettano la capacità: ", capacity_respected)
-            println("Tutte le rotte rispettano le finestre temporali: ", time_windows_respected, ", n violazioni: ", count(route -> !route.time_windows_respected, population.best_individual.routes))
-            println("Tutte le rotte tornano prima del tempo massimo: ", return_time_respected)
-            
+            println("All routes respect capacity: ", capacity_respected)
+            println("All routes respect time windows: ", time_windows_respected, ", number of violations: ", count(route -> !route.time_windows_respected, population.best_individual.routes))
+            println("All routes return before the maximum time: ", return_time_respected)
+
             #plot_routes(problem.depot, population.best_individual.routes)
         end
         # CROSSOVER
         crossover_pop!(population, problem.travel_times)
         # MUTATION
         adaptive_mutation!(fitness_history,
-            N_SWAP, N_INVERSION, N_SHIFT, PERC_SPLIT_MUTATION,
-            N_SWAP_CURR, N_INVERSION_CURR, N_SHIFT_CURR, PERC_SPLIT_MUTATION_CURR)
-        apply_mutation!(population, N_SWAP_CURR, N_INVERSION_CURR, N_SHIFT_CURR, PERC_SPLIT_MUTATION_CURR)
+            N_MOVE, N_SWAP, N_INVERSION, N_SHIFT, PERC_SPLIT_MUTATION,
+            N_MOVE_CURR, N_SWAP_CURR, N_INVERSION_CURR, N_SHIFT_CURR, PERC_SPLIT_MUTATION_CURR)
+        apply_mutation!(population, N_MOVE_CURR, N_SWAP_CURR, N_INVERSION_CURR, N_SHIFT_CURR, PERC_SPLIT_MUTATION_CURR)
         update_population_fitness!(population, problem)
-        # SELEZIONE SURVIVORS
+        # SURVIVORS SELECTION
         elitism_tournament_survivor_selection!(population, TOURNAMENT_SIZE)
         # MAPPA I PROGRESSI
+        push_std_dev_fitness!(population)
         push_mean_fitness!(population)
         push_min_fitness!(population)
         push!(fitness_history, population.mean_fitness[end])
     end
     plot_routes(problem.depot, population.best_individual.routes)
     plot_fitness_evolution(population)
-    #just_mutation!(population, problem, 10_000, 3, 1, 4, 0.1)
+    just_mutation!(population, problem, 10_000, 1, 3, 1, 4, 0.1)
     return population.best_individual
 end
 
-function just_mutation!(population, problem, N_ITER, N_SWAP_CURR, N_INVERSION_CURR, N_SHIFT_CURR, PERC_SPLIT_MUTATION_CURR)
+function just_mutation!(population, problem, N_ITER, N_MOVE, N_SWAP_CURR, N_INVERSION_CURR, N_SHIFT_CURR, PERC_SPLIT_MUTATION_CURR)
     for iter in 1:N_ITER 
         if iter % 100 == 0
             println("Iterazione: ", iter)
@@ -59,11 +62,12 @@ function just_mutation!(population, problem, N_ITER, N_SWAP_CURR, N_INVERSION_CU
             println("Best fitness: ", population.best_individual.fitness, ", feasible: ", population.best_individual.feasible)
         end
         # MUTATION
-        apply_mutation!(population, N_SWAP_CURR, N_INVERSION_CURR, N_SHIFT_CURR, PERC_SPLIT_MUTATION_CURR)
+        apply_mutation!(population, N_MOVE, N_SWAP_CURR, N_INVERSION_CURR, N_SHIFT_CURR, PERC_SPLIT_MUTATION_CURR)
         update_population_fitness!(population, problem)
         # SELEZIONE SURVIVORS
         elitism!(population)
         # MAPPA I PROGRESSI
+        push_std_dev_fitness!(population)
         push_mean_fitness!(population)
         push_min_fitness!(population)
     end
