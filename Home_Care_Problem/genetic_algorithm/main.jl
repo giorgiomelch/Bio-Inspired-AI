f871e03c9347f46b8a027d4036917e59c52b26b8
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-03-07 17:50:42 +0100

    solo crossover

--- Home_Care_Problem/genetic_algorithm/main.jl
+++ Home_Care_Problem/genetic_algorithm/main.jl
@@ -13,22 +13,22 @@ using Statistics
 #   COSTANTS  #
 ###############
 # TERMINATION CRITERION
-N_ITER = 150
+N_ITER = 70
 # POP CONSTANTS
 N_POP = 500
 # TORUNAMENT SELECTION CONSTANTS
 TOURNAMENT_SIZE = 4
 # CROSSOVER CONSTANTS
 # MUTATION COSTANTS
-N_MOVE = 1
-N_SWAP_MUTATION = 1
-N_INVERSION = 1
-N_SHIFT = 1
-PERC_SPLIT_MUTATION = 0.1
+N_MOVE = 0
+N_SWAP_MUTATION = 0
+N_INVERSION = 0
+N_SHIFT = 0
+PERC_SPLIT_MUTATION = 0.0
 
-for data_train_nbr in 0:2
+for data_train_nbr in 1:2
         HCP = load_home_care_problem("/home/giorgiomelch/BI_AI/workspace/genetic-algorithm/Home_Care_Problem/test/test_"* string(data_train_nbr) *".json")
-
+        
         @time best_individual = genetic_algorithm(HCP, N_POP, 
                 N_ITER,
                 TOURNAMENT_SIZE,
@@ -38,4 +38,6 @@ for data_train_nbr in 0:2
         println("Best fitness: ", best_individual.fitness, ", feasible: ", best_individual.feasible, "\nis_back_before_return_time: ", all(r -> r.is_back_before_return_time, best_individual.routes), "\ncapacity_respected: ", all(r -> r.capacity_respected, best_individual.routes), "\ntime_windows_respected: ", all(r -> r.time_windows_respected, best_individual.routes))
         println("\nBenchmark: ", HCP.benchmark)
         save_individual_routes(best_individual, "./solution_"*string(data_train_nbr)*".txt", "/home/giorgiomelch/BI_AI/workspace/genetic-algorithm/Home_Care_Problem/TEST_solutions")
+        show_solution(best_individual, HCP)
+        save_show_solution(best_individual, HCP)
 end
