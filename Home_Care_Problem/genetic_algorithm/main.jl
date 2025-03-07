74a35f03cd6e726857789102f334a9bb64b4c6e5
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-03-07 10:44:57 +0100

    N_MOVE

--- Home_Care_Problem/genetic_algorithm/main.jl
+++ Home_Care_Problem/genetic_algorithm/main.jl
@@ -17,27 +17,25 @@ N_ITER = 150
 # POP CONSTANTS
 N_POP = 500
 # TORUNAMENT SELECTION CONSTANTS
-TOURNAMENT_SIZE = 3
+TOURNAMENT_SIZE = 4
 # CROSSOVER CONSTANTS
 # MUTATION COSTANTS
+N_MOVE = 1
 N_SWAP_MUTATION = 1
 N_INVERSION = 1
 N_SHIFT = 1
 PERC_SPLIT_MUTATION = 0.1
 
-data_train_nbr = 0
 for data_train_nbr in 0:2
-        #HCP = load_home_care_problem("/home/giorgiomelch/BI_AI/workspace/genetic-algorithm/Home_Care_Problem/data/train_"* string(data_train_nbr) *".json")HCP = load_home_care_problem("/home/giorgiomelch/BI_AI/workspace/genetic-algorithm/Home_Care_Problem/data/train_"* string(data_train_nbr) *".json")
         HCP = load_home_care_problem("/home/giorgiomelch/BI_AI/workspace/genetic-algorithm/Home_Care_Problem/test/test_"* string(data_train_nbr) *".json")
 
         @time best_individual = genetic_algorithm(HCP, N_POP, 
                 N_ITER,
                 TOURNAMENT_SIZE,
-                N_SWAP_MUTATION, N_INVERSION, N_SHIFT, PERC_SPLIT_MUTATION)
+                N_MOVE, N_SWAP_MUTATION, N_INVERSION, N_SHIFT, PERC_SPLIT_MUTATION)
 
         print_individual_routes(best_individual)
         println("Best fitness: ", best_individual.fitness, ", feasible: ", best_individual.feasible, "\nis_back_before_return_time: ", all(r -> r.is_back_before_return_time, best_individual.routes), "\ncapacity_respected: ", all(r -> r.capacity_respected, best_individual.routes), "\ntime_windows_respected: ", all(r -> r.time_windows_respected, best_individual.routes))
-
         println("\nBenchmark: ", HCP.benchmark)
         save_individual_routes(best_individual, "./solution_"*string(data_train_nbr)*".txt", "/home/giorgiomelch/BI_AI/workspace/genetic-algorithm/Home_Care_Problem/TEST_solutions")
 end
