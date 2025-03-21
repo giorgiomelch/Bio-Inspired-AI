51fc1d19d53a5e65fb4e557dd6da1b380bd2cdda
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-03-21 18:33:33 +0100

    implementato un semplice GA

---
+++ Understanding_Optimization_Landscapes/src/algorithms/Simple_Genetic_Algorithm.jl
@@ -0,0 +1,38 @@
+include("../fitness_function.jl")
+include("_initialization.jl")
+include("_selection.jl")
+include("_crossover.jl")
+include("_mutation.jl")
+
+
+function simple_genetic_algorithm(X, y, POPULATION_SIZE, N_ITERATIONS, MUTATION_RATE)
+    N_GENES = size(X, 2)
+    population = initialize_random_population(POPULATION_SIZE, N_GENES)
+    fitness = calculate_population_fitness(X, y, population)
+    best_individual = population[argmax(fitness), :]
+    best_fitness = maximum(fitness)
+    for i in 1:N_ITERATIONS
+        # PARENT SELECTION FOR CROSSOVER
+        parents = tournament_selection(population, fitness, POPULATION_SIZE, 3)
+        # CROSSOVER
+        children = two_point_crossover(parents)
+        population = vcat(population, children)
+        # MUTATION
+        mutation_bit_flip!(population, MUTATION_RATE)
+        # EVALUATE FITNESS
+        fitness = calculate_population_fitness(X, y, population)
+        if maximum(fitness) > best_fitness
+            best_individual = population[argmax(fitness), :]
+            best_fitness = maximum(fitness)
+        end
+        # SURVIVOR SELECTION
+        println("POP N1", size(population))
+        population, fitness = elitism(population, fitness, POPULATION_SIZE)
+        println("POP N2", size(population))
+        
+        println(population)
+    end
+    return best_individual, best_fitness
+end
+
+simple_genetic_algorithm(15, 10, 11, 0.8)
