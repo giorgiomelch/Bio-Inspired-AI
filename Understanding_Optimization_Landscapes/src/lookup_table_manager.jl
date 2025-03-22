63924123f218507b9ff2231b77b3e0a9d2aa7778
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-03-22 12:02:32 +0100

    file che gestisce lookup tables

---
+++ Understanding_Optimization_Landscapes/src/lookup_table_manager.jl
@@ -0,0 +1,26 @@
+using Serialization
+
+function build_lookup_table(features_number::Int)
+    total_combinations = 2^features_number
+    lookup_table = Vector{Float64}(undef, total_combinations)
+    for i in 1:(total_combinations)
+        lookup_table[i] = -1.0
+    end
+    return lookup_table
+end
+
+function update_lookup_table!(lookup_table::Vector{Float64}, index::Int, new_value::Float64)
+    if index < 1 || idx > length(lookup_table)
+        error("Combinazione fuori range: l'indice calcolato è $index, mentre la tabella ha dimensione $(length(lookup_table))")
+    end
+    lookup_table[index] = new_value
+    return lookup_table
+end
+
+function save_lookup_table(lookup_table::Vector{Float64}, filename::String)
+    serialize(filename, lookup_table)
+end
+
+function load_lookup_table(filename::String)
+    return deserialize(filename)
+end
