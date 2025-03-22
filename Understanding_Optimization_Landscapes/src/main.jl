9c9667d7f8bb229decace65b30e1d50f40e1d6e7
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-03-22 14:12:27 +0100

    sfrutta la lookout table

--- Understanding_Optimization_Landscapes/src/main.jl
+++ Understanding_Optimization_Landscapes/src/main.jl
@@ -8,12 +8,19 @@ include("./algorithms/Simple_Genetic_Algorithm.jl")
 
 using CSV, DataFrames, Random, Statistics
 
+# LOAD DATASET
 winequality_white_data_path = joinpath(@__DIR__, "..", "data", "winequality-white.csv")
-student_performance_data_path = joinpath(@__DIR__, "..", "data", "Student_performance_data.csv")
+student_performance_data_path = joinpath(@__DIR__, "..", "data", "Student_performance_data_modified.csv")
 online_shoppers_intention_data_path = joinpath(@__DIR__, "..", "data", "online_shoppers_intention.csv")
+# LOAD LOOKUP TABLE
+lookup_table = load_lookup_table("lookup_table_student_performance.jls")
+
 
 df = CSV.read(student_performance_data_path, DataFrame; header=true, delim=',')
 X = Matrix(select(df, Not(last(names(df)))))
 y = df[!, last(names(df))]  
 
-simple_genetic_algorithm(X, y, 15, 20, 0.8)
+@time simple_genetic_algorithm(X, y, lookup_table, 15, 5, 0.8)
+
+# SAVE LOOKUP TABLE
+save_lookup_table(lookup_table, "lookup_table_student_performance.jls")
