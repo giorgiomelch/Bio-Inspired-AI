1aa01eaf2af6c12d871c8ed7582823d3175f99a8
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-03-24 10:02:00 +0100

    mostra andamento

--- Understanding_Optimization_Landscapes/src/algorithms/Simple_Genetic_Algorithm.jl
+++ Understanding_Optimization_Landscapes/src/algorithms/Simple_Genetic_Algorithm.jl
@@ -1,24 +1,27 @@
 include("../fitness_function.jl")
+include("../plotter.jl")
 include("_initialization.jl")
 include("_selection.jl")
 include("_crossover.jl")
 include("_mutation.jl")
 
-
 function simple_genetic_algorithm(X, y, lookup_table, POPULATION_SIZE, N_ITERATIONS, MUTATION_RATE)
+    mean_fitness = Float64[]
+    max_fitness = Float64[]
     N_GENES = size(X, 2)
     population = initialize_random_population(POPULATION_SIZE, N_GENES)
     fitness = calculate_population_fitness(X, y, population, lookup_table)
     best_individual = population[argmax(fitness), :]
     best_fitness = maximum(fitness)
     for i in 1:N_ITERATIONS
+        println("ITERATION N: ", i)
         # PARENT SELECTION FOR CROSSOVER
         parents = tournament_selection(population, fitness, POPULATION_SIZE, 3)
         # CROSSOVER
-        children = two_point_crossover(parents)
-        population = vcat(population, children)
+        offsprings = two_point_crossover(parents)
         # MUTATION
-        mutation_bit_flip!(population, MUTATION_RATE)
+        mutation_bit_flip!(offsprings, MUTATION_RATE)
+        population = vcat(population, offsprings)
         # EVALUATE FITNESS
         fitness = calculate_population_fitness(X, y, population, lookup_table)
         if maximum(fitness) > best_fitness
@@ -27,6 +30,9 @@ function simple_genetic_algorithm(X, y, lookup_table, POPULATION_SIZE, N_ITERATI
         end
         # SURVIVOR SELECTION
         population, fitness = elitism(population, fitness, POPULATION_SIZE)
+        push!(mean_fitness, mean(fitness))
+        push!(max_fitness, maximum(fitness))
     end
+    plot_fitness_evolution(mean_fitness, max_fitness)
     return best_individual, best_fitness
 end
