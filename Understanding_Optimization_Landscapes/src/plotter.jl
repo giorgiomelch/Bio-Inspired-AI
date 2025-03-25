aab25a5380d67f3cf31e572d75399acf49a57dcf
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-03-25 20:06:53 +0100

    plot humming distance

--- Understanding_Optimization_Landscapes/src/plotter.jl
+++ Understanding_Optimization_Landscapes/src/plotter.jl
@@ -1,19 +1,27 @@
 using Plots
 
 function plot_fitness_evolution(mean_fitness::Vector{Float64}, minimum_fitness::Vector{Float64})
-    p = plot(1:length(mean_fitness), mean_fitness, label="Mean accuracy", lw=2, color=:blue, linestyle=:solid)
-    plot!(p, 1:length(minimum_fitness), minimum_fitness, label="Minimum accuracy", lw=2, color=:green, linestyle=:solid)
+    p = plot(1:length(mean_fitness), mean_fitness, label="Mean fitness", lw=2, color=:blue, linestyle=:solid)
+    plot!(p, 1:length(minimum_fitness), minimum_fitness, label="Minimum fitness", lw=2, color=:green, linestyle=:solid)
     xlabel!(p, "Iteration")
     ylabel!(p, "Fitness")
     title!(p, "Fitness Evolution")
     display(p)
 end
 
-function plot_NSGA2_population(fitness::Vector{Float64}, n_feature_used::Vector{Int})
-    p = plot(n_feature_used, fitness, seriestype=:scatter, 
-        xlabel="Numero di Feature Usate", 
-        ylabel="Fitness (Error+Penalty)", 
-        title="Distribuzione della Popolazione", 
+function plot_humming_distance_evolution(distance::Vector{Float64})
+    p = plot(1:length(distance), distance, label="Hamming distance", lw=2, color=:grey, linestyle=:solid)
+    xlabel!(p, "Iteration")
+    ylabel!(p, "Hamming distance")
+    title!(p, "Hamming distance Evolution")
+    display(p)
+end
+
+function plot_NSGA2_population(errors::Vector{Float64}, n_feature_used::Vector{Int})
+    p = plot(n_feature_used, errors, seriestype=:scatter, 
+        xlabel="Number of features used", 
+        ylabel="Error", 
+        title="Distribution of the population", 
         legend=false, 
         markersize=5, 
         markercolor=:blue, 
