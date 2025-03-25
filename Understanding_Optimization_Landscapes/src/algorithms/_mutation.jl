86dcefd91e8d0aad1ed56e0cc9015c5c69c29af0
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-03-25 13:15:18 +0100

    bug resolved

--- Understanding_Optimization_Landscapes/src/algorithms/_mutation.jl
+++ Understanding_Optimization_Landscapes/src/algorithms/_mutation.jl
@@ -1,11 +1,13 @@
-function mutation_bit_flip!(population, bit_flip__probability)
+function mutation_bit_flip!(population, bit_flip_probability)
     population_length, number_of_genes = size(population)
-    # Iterate for every individual in the population
     for i in 1:population_length
-        gene_to_mutate = rand(1:number_of_genes)
         # Apply bit flip mutation with a given probability
-        if rand() < bit_flip__probability
-            population[i, gene_to_mutate] = mod(population[i, gene_to_mutate] + 1, 1)
+        if rand() < bit_flip_probability
+            gene_to_mutate = rand(1:number_of_genes)
+            population[i, gene_to_mutate] = 1 - population[i, gene_to_mutate]
+        end
+    end
+end
         end
     end
 end
