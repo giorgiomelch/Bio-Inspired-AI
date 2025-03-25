a424a5c730f7db19b602c7dda0c42f02bc5d892e
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-03-25 20:06:05 +0100

    humming distance + passa accuracy e n features

--- Understanding_Optimization_Landscapes/src/algorithms/NSGA2.jl
+++ Understanding_Optimization_Landscapes/src/algorithms/NSGA2.jl
@@ -4,17 +4,15 @@ include("_initialization.jl")
 include("_selection.jl")
 include("_crossover.jl")
 include("_mutation.jl")
+include("_distance.jl")
 
-function calculate_n_feature_used(population)
-    n_features = sum(population, dims=2)
-    return vec(n_features)
-end
 
 function NSGA2(lookup_table, N_FEATURES, POPULATION_SIZE, N_ITERATIONS, MUTATION_RATE)
     mean_fitness = Float64[]
     minimum_fitness = Float64[]
+    humming_distance = Float64[]
     population = initialize_random_population(POPULATION_SIZE, N_FEATURES)
-    fitness = calculate_population_fitness(population, lookup_table)
+    fitness, accuracy = calculate_population_fitness(population, lookup_table)
     best_individual = population[argmin(fitness), :]
     best_fitness = minimum(fitness)
     for i in 1:N_ITERATIONS
@@ -26,20 +24,23 @@ function NSGA2(lookup_table, N_FEATURES, POPULATION_SIZE, N_ITERATIONS, MUTATION
         mutation_bit_flip!(offsprings, MUTATION_RATE)
         population = vcat(population, offsprings)
         # EVALUATE FITNESS
-        fitness = calculate_population_fitness(population, lookup_table)
+        fitness, accuracy = calculate_population_fitness(population, lookup_table)
         if minimum(fitness) < best_fitness
             best_individual = population[argmin(fitness), :]
             best_fitness = minimum(fitness)
         end
-        n_feature_used = calculate_n_feature_used(population)
         # SURVIVOR SELECTION
-        plot_NSGA2_population(fitness, n_feature_used)
-        population, fitness = nsga_selection(population, fitness, n_feature_used, POPULATION_SIZE)
-        n_feature_used = calculate_n_feature_used(population)
-        plot_NSGA2_population(fitness, n_feature_used)
+        n_feature_used = vec(sum(population, dims=2))
+        population, accuracy, fitness, n_feature_used = nsga_selection(population, accuracy, fitness, n_feature_used, POPULATION_SIZE)
         push!(mean_fitness, mean(fitness))
         push!(minimum_fitness, minimum(fitness))
+        push!(humming_distance, average_hamming_distance(population))
+        if i % 2 == 0 && i != 0
+            println("ITERATION N: ", i, ", mean fitness: ", round(mean(fitness), digits=3), ", minimum fitness: ", round(minimum(fitness), digits=3))
+            plot_NSGA2_population(fitness, n_feature_used)
+        end
     end
+    plot_humming_distance_evolution(humming_distance)
     plot_fitness_evolution(mean_fitness, minimum_fitness)
-    return best_individual, best_fitness
+    return best_fitness, best_individual
 end
