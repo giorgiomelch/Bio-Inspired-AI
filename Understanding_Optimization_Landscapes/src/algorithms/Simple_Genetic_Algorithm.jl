16e04cf5a2d26a93724657d712b0046f97f59344
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-03-25 13:14:05 +0100

    just use lookuptable and update to minimization

--- Understanding_Optimization_Landscapes/src/algorithms/Simple_Genetic_Algorithm.jl
+++ Understanding_Optimization_Landscapes/src/algorithms/Simple_Genetic_Algorithm.jl
@@ -5,34 +5,35 @@ include("_selection.jl")
 include("_crossover.jl")
 include("_mutation.jl")
 
-function simple_genetic_algorithm(X, y, lookup_table, POPULATION_SIZE, N_ITERATIONS, MUTATION_RATE)
+function simple_genetic_algorithm(lookup_table, N_FEATURES, POPULATION_SIZE, N_ITERATIONS, MUTATION_RATE)
     mean_fitness = Float64[]
-    max_fitness = Float64[]
-    N_GENES = size(X, 2)
-    population = initialize_random_population(POPULATION_SIZE, N_GENES)
-    fitness = calculate_population_fitness(X, y, population, lookup_table)
+    minimum_fitness = Float64[]
+    population = initialize_random_population(POPULATION_SIZE, N_FEATURES)
+    fitness, accuracy = calculate_population_fitness(population, lookup_table)
     best_individual = population[argmax(fitness), :]
     best_fitness = maximum(fitness)
     for i in 1:N_ITERATIONS
-        println("ITERATION N: ", i)
+        if i % 10 == 0 && i != 0
+            println("ITERATION N: ", i, " mean fitness: ", round(mean(fitness), digits=3), " minimum fitness: ", round(minimum(fitness), digits=3))
+        end
         # PARENT SELECTION FOR CROSSOVER
-        parents = tournament_selection(population, fitness, POPULATION_SIZE, 3)
+        parents = tournament_selection(population, fitness, POPULATION_SIZE, 2)
         # CROSSOVER
         offsprings = two_point_crossover(parents)
         # MUTATION
         mutation_bit_flip!(offsprings, MUTATION_RATE)
         population = vcat(population, offsprings)
         # EVALUATE FITNESS
-        fitness = calculate_population_fitness(X, y, population, lookup_table)
-        if maximum(fitness) > best_fitness
-            best_individual = population[argmax(fitness), :]
-            best_fitness = maximum(fitness)
+        fitness, accuracy = calculate_population_fitness(population, lookup_table)
+        if minimum(fitness) < best_fitness
+            best_individual = population[argmin(fitness), :]
+            best_fitness = minimum(fitness)
         end
         # SURVIVOR SELECTION
         population, fitness = elitism(population, fitness, POPULATION_SIZE)
         push!(mean_fitness, mean(fitness))
-        push!(max_fitness, maximum(fitness))
+        push!(minimum_fitness, minimum(fitness))
     end
-    plot_fitness_evolution(mean_fitness, max_fitness)
-    return best_individual, best_fitness
+    plot_fitness_evolution(mean_fitness, minimum_fitness)
+    return best_fitness, best_individual
 end
