a45d7dafd1a08ddd2b8b61f0ebf8037bf74d8896
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-03-25 13:14:51 +0100

    minimization problem

--- Understanding_Optimization_Landscapes/src/plotter.jl
+++ Understanding_Optimization_Landscapes/src/plotter.jl
@@ -1,8 +1,8 @@
 using Plots
 
-function plot_fitness_evolution(mean_fitness::Vector{Float64}, max_fitness::Vector{Float64})
+function plot_fitness_evolution(mean_fitness::Vector{Float64}, minimum_fitness::Vector{Float64})
     p = plot(1:length(mean_fitness), mean_fitness, label="Mean accuracy", lw=2, color=:blue, linestyle=:solid)
-    plot!(p, 1:length(max_fitness), max_fitness, label="Maximum accuracy", lw=2, color=:green, linestyle=:solid)
+    plot!(p, 1:length(minimum_fitness), minimum_fitness, label="Minimum accuracy", lw=2, color=:green, linestyle=:solid)
     xlabel!(p, "Iteration")
     ylabel!(p, "Fitness")
     title!(p, "Fitness Evolution")
@@ -12,7 +12,7 @@ end
 function plot_NSGA2_population(fitness::Vector{Float64}, n_feature_used::Vector{Int})
     p = plot(n_feature_used, fitness, seriestype=:scatter, 
         xlabel="Numero di Feature Usate", 
-        ylabel="Fitness (Accuratezza)", 
+        ylabel="Fitness (Error+Penalty)", 
         title="Distribuzione della Popolazione", 
         legend=false, 
         markersize=5, 
