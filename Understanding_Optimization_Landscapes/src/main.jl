c488b6e379c6410986f7434dc68c046fe0e730a2
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-03-25 10:39:45 +0100

    update

--- Understanding_Optimization_Landscapes/src/main.jl
+++ Understanding_Optimization_Landscapes/src/main.jl
@@ -7,18 +7,21 @@ using CSV, DataFrames, Random, Statistics
 
 # LOAD DATASET
 winequality_white_data_path = joinpath(@__DIR__, "..", "data", "winequality-white.csv")
-student_performance_data_path = joinpath(@__DIR__, "..", "data", "Student_performance_data.csv")
+ObesityDataSet_raw_and_data_sinthetic_data_path = joinpath(@__DIR__, "..", "data", "ObesityDataSet_raw_and_data_sinthetic.csv")
 online_shoppers_intention_data_path = joinpath(@__DIR__, "..", "data", "online_shoppers_intention.csv")
+
+# CREATE LOOKUP TABLE - commented beacuse it takes a lot of time
+#create_lookup_table(X, y, 16, "lookup_table_student_performance.jls")
+#create_lookup_table(X, y, 16, "winequality_white_data_path.jls")
+#create_lookup_table(X, y, 16, "ObesityDataSet_raw_and_data_sinthetic.jls")
+
 # LOAD LOOKUP TABLE
-lookup_table = load_lookup_table("lookup_table_student_performance.jls")
+lookup_table = load_lookup_table("ObesityDataSet_raw_and_data_sinthetic.jls")
 
-
-df = CSV.read(student_performance_data_path, DataFrame; header=true, delim=',')
+df = CSV.read(ObesityDataSet_raw_and_data_sinthetic_data_path, DataFrame; header=true, delim=',')
 X = Matrix(select(df, Not(last(names(df)))))
 y = df[!, last(names(df))]  
 
-#@time simple_genetic_algorithm(X, y, lookup_table, 10, 10, 0.8)
-@time NSGA2(X, y, lookup_table, 5, 10, 0.8)
 
-# SAVE LOOKUP TABLE
-save_lookup_table(lookup_table, "lookup_table_student_performance.jls")
+@time simple_genetic_algorithm(lookup_table, 16, 10, 10, 0.8)
+#@time NSGA2(X, y, lookup_table, 10, 5, 0.8)
