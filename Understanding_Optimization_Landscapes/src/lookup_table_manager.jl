1a22972fa3206a591c142ef903ea53447d43e35b
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-03-25 10:38:14 +0100

    crea una lookup table

--- Understanding_Optimization_Landscapes/src/lookup_table_manager.jl
+++ Understanding_Optimization_Landscapes/src/lookup_table_manager.jl
@@ -1,4 +1,6 @@
 using Serialization
+using Base.Threads
+include("fitness_function.jl")
 
 function build_lookup_table(features_number::Int)
     total_combinations = 2^features_number
@@ -6,14 +8,6 @@ function build_lookup_table(features_number::Int)
     for i in 1:(total_combinations)
         lookup_table[i] = -1.0
     end
-    return lookup_table
-end
-
-function update_lookup_table!(lookup_table::Vector{Float64}, index::Int, new_value::Float64)
-    if index < 1 || idx > length(lookup_table)
-        error("Combinazione fuori range: l'indice calcolato è $index, mentre la tabella ha dimensione $(length(lookup_table))")
-    end
-    lookup_table[index] = new_value
     return lookup_table
 end
 
@@ -25,11 +19,24 @@ function load_lookup_table(filename::String)
     filepath = joinpath(@__DIR__, "lookup_tables", filename)
     return deserialize(filepath)
 end
-using Serialization
 
 function save_lookup_table(lookup_table::Vector{Float64}, filename::String)
     save_dir = joinpath(@__DIR__, "lookup_tables")
     filepath = joinpath(save_dir, filename)
     serialize(filepath, lookup_table)
     println("Lookup table salvata in: ", filepath)
+end
+
+function create_lookup_table(X, y, features_number::Int, filename::String)
+    lookup_table = build_lookup_table(features_number)
+    count = 0
+    @threads for i in 1:(2^features_number - 1)
+        features_used = [parse(Int, x) for x in string(i, base=2, pad=features_number)]
+        accuracy = random_forest(features_used, X, y)
+        index_lt = features_to_index(features_used)
+        lookup_table[index_lt] = accuracy
+        println("Iter n: ", count, "/", 2^features_number, " - i: ", i, "(", index_lt, ")", "features_used: ", features_used, ", accuracy: ", accuracy)
+        count += 1
+    end
+    save_lookup_table(lookup_table, filename)
 end
