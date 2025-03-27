5128e29ed74c7a0d2641b7ba48d5673bb3f48a78
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-03-27 17:38:52 +0100

    if individual has just zeros flip one random

--- Understanding_Optimization_Landscapes/src/algorithms/_mutation.jl
+++ Understanding_Optimization_Landscapes/src/algorithms/_mutation.jl
@@ -6,5 +6,8 @@ function mutation_bit_flip!(population, bit_flip_probability)
             gene_to_mutate = rand(1:number_of_genes)
             population[i, gene_to_mutate] = 1 - population[i, gene_to_mutate]
         end
+        if sum(population[i, :]) == 0
+            population[i, rand(1:number_of_genes)] = 1
+        end
     end
 end
