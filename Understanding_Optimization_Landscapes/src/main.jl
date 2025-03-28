86987db8a6a188063ac814dca2d5c096643885a7
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-03-28 18:52:05 +0100

    update

--- Understanding_Optimization_Landscapes/src/main.jl
+++ Understanding_Optimization_Landscapes/src/main.jl
@@ -2,30 +2,41 @@ include("fitness_function.jl")
 include("lookup_table_manager.jl")
 include("./algorithms/Simple_Genetic_Algorithm.jl")
 include("./algorithms/NSGA2.jl")
+include("./algorithms/Particle_Swarm_Optimization.jl")
 
 using CSV, DataFrames, Random, Statistics
 
+path1 = "winequality-white"
+path2 = "ObesityDataSet_raw_and_data_sinthetic"
+path3 = "online_shoppers_intention"
 # LOAD DATASET
-winequality_white_data_path = joinpath(@__DIR__, "..", "data", "winequality-white.csv")
-ObesityDataSet_raw_and_data_sinthetic_data_path = joinpath(@__DIR__, "..", "data", "ObesityDataSet_raw_and_data_sinthetic.csv")
-online_shoppers_intention_data_path = joinpath(@__DIR__, "..", "data", "online_shoppers_intention.csv")
-
-# LOAD LOOKUP TABLE
-lookup_table = load_lookup_table("ObesityDataSet_raw_and_data_sinthetic.jls")
-
-df = CSV.read(ObesityDataSet_raw_and_data_sinthetic_data_path, DataFrame; header=true, delim=',')
-X = Matrix(select(df, Not(last(names(df)))))
-y = df[!, last(names(df))]  
-# CREATE LOOKUP TABLE - commented beacuse it takes a lot of time
+path = path2
+data_path = joinpath(@__DIR__, "..", "data", path * ".csv")
+# CREATE LOOKUP TABLE - commented because it takes a lot of time
 #create_lookup_table(X, y, 16, "lookup_table_student_performance.jls")
-#create_lookup_table(X, y, 11, "winequality_white_data_path.jls")
+#create_lookup_table(X, y, 11, "winequality-white.jls")
 #create_lookup_table(X, y, 16, "ObesityDataSet_raw_and_data_sinthetic.jls")
 
+# LOAD LOOKUP TABLE
+lookup_table = load_lookup_table(path * ".jls")
+# LOAD DATASET
+df = CSV.read(data_path, DataFrame; header=true, delim=';')
+X = Matrix(select(df, Not(last(names(df)))))
+y = df[!, last(names(df))]  
+number_of_features = size(X, 2)
 
-# BENCHMARK TO REACH
-#println("Max accuracy: ", maximum(lookup_table), ", Best features: ", reverse(digits(findall(x -> x == maximum(lookup_table), lookup_table)[1], base=2)))
+# SHOW THE BEST ACHIEVABLE ACCURACY 
+println("Best ACCURACY: ", maximum(lookup_table), ", Best features: ", reverse(digits(findall(x -> x == maximum(lookup_table), lookup_table)[1], base=2)))
+# SHOW THE BEST ACHIEVABLE FITNESS
+problem_best_fitness, problem_best_features = find_best_fitness(lookup_table, number_of_features)
+println("Best FITNESS: ", problem_best_fitness, ", with features: ", problem_best_features)
 
-best_fitness, best_individual = simple_genetic_algorithm(lookup_table, 16, 10, 100, 1)
-#best_fitness, best_individual = NSGA2(lookup_table, 16, 10, 100, 1)
-println("RF: ",random_forest(best_individual, X, y))
-println("LT: ",lookup_table[features_to_index(best_individual)])
+# RUN BIOLOGICAL ALGORITHMS
+best_fitness, best_individual = simple_genetic_algorithm(lookup_table, number_of_features, 20, 50, 0.8)
+println("SGA:   accuracy=",lookup_table[features_to_index(best_individual)], ", fitness=", best_fitness, ", features used=",best_individual)
+
+best_fitness, best_individual = NSGA2(lookup_table, number_of_features, 20, 50, 0.8)
+println("NSGA2: accuracy=",lookup_table[features_to_index(best_individual)], ", fitness=", best_fitness, ", features used=",best_individual)
+
+best_features, best_fitness = particle_swarm_optimization(20, number_of_features, 50, 0.7, 1.5, 1.5, lookup_table)
+println("PSO:   accuracy=",lookup_table[features_to_index(best_individual)], ", fitness=", best_fitness, ", features used=",best_individual)
