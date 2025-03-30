5d777986c61bd318d831ea604e622507463e05d9
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-03-30 13:57:04 +0200

    update

--- Understanding_Optimization_Landscapes/src/main.jl
+++ Understanding_Optimization_Landscapes/src/main.jl
@@ -8,22 +8,23 @@ using CSV, DataFrames, Random, Statistics
 
 path1 = "winequality-white"
 path2 = "ObesityDataSet_raw_and_data_sinthetic"
-path3 = "online_shoppers_intention"
+path3 = "magic04"
 # LOAD DATASET
 path = path2
 data_path = joinpath(@__DIR__, "..", "data", path * ".csv")
-# CREATE LOOKUP TABLE - commented because it takes a lot of time
-#create_lookup_table(X, y, 16, "lookup_table_student_performance.jls")
-#create_lookup_table(X, y, 11, "winequality-white.jls")
-#create_lookup_table(X, y, 16, "ObesityDataSet_raw_and_data_sinthetic.jls")
 
-# LOAD LOOKUP TABLE
-lookup_table = load_lookup_table(path * ".jls")
 # LOAD DATASET
 df = CSV.read(data_path, DataFrame; header=true, delim=';')
 X = Matrix(select(df, Not(last(names(df)))))
 y = df[!, last(names(df))]  
 number_of_features = size(X, 2)
+
+# CREATE LOOKUP TABLE - commented because it takes a lot of time
+#create_lookup_table(X, y, 16, "lookup_table_student_performance.jls")
+#create_lookup_table(X, y, 11, "winequality-white.jls")
+#create_lookup_table(X, y, 10, "magic04.jls")
+# LOAD LOOKUP TABLE
+lookup_table = load_lookup_table(path * ".jls")
 
 # SHOW THE BEST ACHIEVABLE ACCURACY 
 println("Best ACCURACY: ", maximum(lookup_table), ", Best features: ", reverse(digits(findall(x -> x == maximum(lookup_table), lookup_table)[1], base=2)))
