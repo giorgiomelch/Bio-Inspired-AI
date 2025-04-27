6d587ef06c09454358d94838dd9ce83b213ca0a8
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2025-04-27 18:21:24 +0200

    removed lines from main

--- Understanding_Optimization_Landscapes/src/step_six.jl
+++ Understanding_Optimization_Landscapes/src/step_six.jl
@@ -12,7 +12,7 @@ using CSV, DataFrames, Random, Statistics
 # -------------------------------------------------------------------------
 dataset_names = Dict(
     "heart"   => "processed_cleveland",
-    "obesity" => "ObesityDataSet_raw_and_data_sinthetic",
+    "zoo"     => "zoo",
     "letters" => "letter_recognition",
 )
 selected_dataset = dataset_names["letters"]
@@ -31,7 +31,7 @@ N_ITERATIONS = 30
 
 function run_algorithm(algorithm_function::Function, algorithm_name::String, lookup_table, num_features, N_POP, N_ITERATIONS, params...)
     Threads.@threads for i in 1:N_RUNS
-        best_fitness, best_individual, convergence = algorithm_function(lookup_table, num_features, N_POP, N_ITERATIONS, params..., -5)
+        best_fitness, best_individual, _ = algorithm_function(lookup_table, num_features, N_POP, N_ITERATIONS, params..., -5)
         println("$algorithm_name: accuracy=", evaluate_individual_step_six(best_individual, X, y), 
                 ", fitness=", best_fitness, 
                 ", features used=", best_individual)
@@ -39,6 +39,6 @@ function run_algorithm(algorithm_function::Function, algorithm_name::String, loo
 end
 
 
-#run_algorithm(simple_genetic_algorithm, "SGA", [], number_of_features, N_POP, N_ITERATIONS, 0.8)
-run_algorithm(NSGA2, "NSGA2", [], number_of_features, N_POP, N_ITERATIONS, 0.8)
+run_algorithm(simple_genetic_algorithm, "SGA", [], number_of_features, N_POP, N_ITERATIONS, 0.8)
+#run_algorithm(NSGA2, "NSGA2", [], number_of_features, N_POP, N_ITERATIONS, 0.8)
 #run_algorithm(particle_swarm_optimization, "PSO", [], number_of_features, N_POP, N_ITERATIONS, 0.9, 2.0, 2.0, 2.0, 2)
